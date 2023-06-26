fafd6fd73a6fe5e3134422dae0c7422b28224826
IterativeOptimization <137118640+IterativeOptimization@users.noreply.github.com>
2023-06-26 18:52:25 -0500

    Add files via upload

--- Iterations/BinaryGap/Java/BinaryGap.java
+++ Iterations/BinaryGap/Java/BinaryGap.java
@@ -1,7 +1,5 @@
-class main
-{
-    public static void main(String[] args)
-    {
+class main {
+    public static void main(String[] args) {
         int input_value = 1041;
         System.out.println("Input Value: " + input_value);
         Solution mySolution = new Solution();
@@ -10,43 +8,38 @@ class main
     }
 }
 
-class Solution
-{
-    public int solution(int N)
-    {
-        if(N <= 1)
-        {
-            return 0;
-        }
-
-        if(N > 2147483647)
-        {
+class Solution {
+    public int solution(int N) {
+        // ensure positive non-zero input
+        if (N <= 1) {
             return 0;
         }
 
         int working_value = N;
         int max_gap = 0;
-        int zero_count = -1;
+        int zero_count = -1;    // -1 indicates that no complete gap has yet been found
 
-        while (working_value > 0)
-        {
-            if ((working_value & 1) == 1)
-            {
-                if (zero_count > max_gap)
-                {
+        // check each bit of the input value
+        while (working_value > 0) {
+            // check if the 1 bit is active
+            if ((working_value & 1) == 1) {
+                // if a gap count was in progress, record it if it is longer than the previous highest
+                if (zero_count > max_gap) {
                     max_gap = zero_count;
                 }
+                // reset the current count
                 zero_count = 0;
-            }
-            else
-            {
-                if (zero_count != -1)
-                {
+            } else {
+                // if the 1 bit is NOT active AND the current bit is a zero
+                if (zero_count != -1) {
+                    // start or continue the count
                     zero_count++;
                 }
             }
+            // advance to the next bit in the sequence
             working_value = working_value >> 1;
         }
+
         return max_gap;
     }
 }
